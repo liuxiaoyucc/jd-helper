1b9872aa29dcac4769cfa178a5988bbfe9062ef1
xyliu <2199842341@qq.com>
2020-06-14 22:30:42 +0800

    浏览精选活动架子,未测试

--- red_packet/index.js
+++ red_packet/index.js
@@ -1,4 +1,6 @@
-var JD_HOST = "https://api.m.jd.com/api?appid=jd_mp_h5&";
+var JD_HOST = "https://api.m.jd.com/client.action?";
+var JD_RED_PARKET_HOST = "https://api.m.jd.com/api?appid=jd_mp_h5&";
+
 
 var taskInfo = null;
 
@@ -8,6 +10,28 @@ function* start() {
 
 // 请求
 async function request(function_id, body = {}) {
+
+	var timestamp = new Date().getTime();
+	return new Promise((resolve, reject) => {
+		fetch(JD_RED_PARKET_HOST + 'functionId=' + function_id +
+				'&loginType=2&client=jd_mp_h5&clientVersion=9.0.0&osVersion=AndroidOS&d_brand=UnknownTablet&d_model=UnknownTablet&t=' +
+				timestamp, {
+					body: JSON.stringify(JSON.stringify(body)),
+					cache: 'no-cache',
+					method: 'POST',
+					credentials: "include",
+				})
+			.then(res => {
+				return res.json();
+			})
+			.then((response) => {
+
+				resolve(response);
+			});
+	})
+}
+
+async function cliectRequest(function_id, body = {}) {
 
 	var timestamp = new Date().getTime();
 	return new Promise((resolve, reject) => {
@@ -44,6 +68,69 @@ function taskHomePage() {
 			step.return();
 		}
 	})
+}
+
+
+
+function getTaskDetailForColor(taskType) {
+	console.log('获取任务详情');
+	
+	return new Promise((resolve, reject)=> {
+		
+		request(arguments.callee.name.toString(), {body: {taskType: taskType + ''}}).then(response => {
+			try {
+				let taskDetail = null;
+				if (response.code == 0 && response.rtn_code == 0 && response.data.biz_code == 0) {
+					taskDetail = response.data.result.advertDetails;
+					console.log('任务详情: ', taskDetail);
+				}
+				resolve(taskDetail);
+			} catch (e) {
+				console.log(e);
+				reject(e);
+				console.log('获取任务详情异常');
+			}
+		})
+		
+	})
+	
+}
+
+function taskReportForColor(taskType, detailId) {
+	console.log('开始完成任务');
+	
+	return new Promise((resolve, reject)=> {
+		
+		request(arguments.callee.name.toString(), {body: {taskType: taskType + '', detailId: detailId}}).then(response => {
+			try {
+				
+				if (response.code == 0 && response.rtn_code == 0 && response.data.biz_code == 0) {
+					let result = response.data.result;
+					console.log('完成任务结果: ', result);
+				}
+				resolve();
+			} catch (e) {
+				console.log(e);
+				reject(e);
+				console.log('完成任务异常');
+			}
+		})
+		
+	})
+}
+
+/**
+ * 浏览8个精选活动任务
+ */
+async function active8() {
+	let actives = await getTaskDetailForColor(4);
+	
+	actives.forEach(active=> {
+		let detail_id = active.id; //通过这个id完成
+		await taskReportForColor(detail_id);
+	})
+	
+	step.next();
 }
 
 var step = start();
